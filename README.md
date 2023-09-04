7e63ce0cdb00f09c6fef147892370b8b7bc98bea
luanfelixlima <luan.felix.lima@outlook.com>
2023-09-04 15:18:04 -0300

    Alterando o tipo de dado carregado nas datas do DF e plotando gráficos para melhor visualização dos dados

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Predição Bitcoin
 
----------------------
+Descrição:\
+Predição Realizada apenas com regressão linear, não considerando fatores externos, apenas estatística.
+
+---
 
 Passos:
 
 Carregamento e Limpeza da base: 
 - Carregamento da base: https://raw.githubusercontent.com/coinmetrics/data/master/csv/btc.csv em um DataFrame com a biblioteca Pandas.
-- 
+- Removendo valores nulos em relação aos preços do bitcoin.
+- Alterando tipos de dados não convenientes.
 
 Exploração e Visualização:
+- Plotando gráficos para melhor visualização da tendência dos preços ao longo dos anos.
+
 
--- main.py
+++ main.py
@@ -1,10 +1,55 @@
 import pandas as pd
+import datetime as dt
 import matplotlib.pyplot as plt
 
 # Carregando dados
 url = 'https://raw.githubusercontent.com/coinmetrics/data/master/csv/btc.csv'
 raw_data = pd.read_csv(url, low_memory=False)
 
-print(raw_data.head())
+dados = raw_data[['time', 'PriceUSD']]
+print("Dados \"Crus\":")
+print(raw_data.head(), '\n')
 
+# Limpeza dados
+print("Dados que vamos usar:")
+print(dados.head(), "\n")
+print("Linhas e colunas antes da limpeza:", dados.shape)
+
+dados = dados.dropna()
+print("Linhas e colunas removendo valores nulos:", dados.shape)  # antes não havia btc
+
+# Verificando os tipos de dados
+print("\nTipo de dados no DF (antes da conversão):\n", dados.dtypes, "\n")
+
+# Alterando o tipo de dados da coluna "time"
+dados['time'] = pd.to_datetime(dados['time'])
+print("Tipo de dados no DF (depois da conversão):\n", dados.dtypes, "\n")
+
+# Plotando gráficos para entender os dados
+datas = list(dados['time'])
+valores = list(dados['PriceUSD'])
+anos = []  # armazenando somente os anos das datas para melhor visualização no gráfico de barras
+for data in datas:
+    anos.append(data.year)
+
+# Grafico de linha
+plt.plot(datas, valores)
+plt.xlabel("Anos")
+plt.ylabel("Preço (USD)")
+plt.title("Valor do Bitcoin x Ano")
+plt.show()
+
+# Grafico de barra
+plt.bar(anos, valores)
+plt.xlabel("Anos")
+plt.ylabel("Preço (USD)")
+plt.title("Valor do Bitcoin x Ano")
+plt.show()
+
+# Grafico de pontos
+plt.scatter(datas, valores)
+plt.xlabel("Anos")
+plt.ylabel("Preço (USD)")
+plt.title("Valor do Bitcoin x Ano")
+plt.show()
 
